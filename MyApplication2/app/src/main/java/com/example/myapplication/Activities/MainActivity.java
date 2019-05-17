cb90dab186eeebd3105d2d322e3c323a55ffc206
m7Rahm <Jack10wil>
2019-05-17 13:30:11 +0400

    movement improvements

--- MyApplication2/app/src/main/java/com/example/myapplication/Activities/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/myapplication/Activities/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.util.SparseArray;
 import android.view.MenuItem;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -14,18 +15,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.View;
-import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
-import android.widget.ExpandableListView;
 import android.widget.TextView;
 
 import com.example.myapplication.Classes.CarInfoClass;
 import com.example.myapplication.Classes.UpdateLocationClass;
 import com.example.myapplication.Classes.UserDataClass;
 import com.example.myapplication.R;
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -39,14 +36,16 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
     private volatile boolean [] hasUpdateFinished;
-    private volatile LatLng latLng;
     private ArrayList<CarInfoClass> carInfoClass;
+    SparseArray<LatLng> markerCoordinates = new SparseArray <>();
+    private static int branchId = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         carInfoClass = (ArrayList<CarInfoClass>) getIntent().getSerializableExtra("carInfoClass");
         UserDataClass userDataClass = (UserDataClass) getIntent().getSerializableExtra("userDataClass");
-        setContentView(R.layout.activity_main);
+        branchId = userDataClass.getUserId();
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -55,7 +54,7 @@ public class MainActivity extends AppCompatActivity
         mapFragment.getMapAsync(this);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-        ExpandableListView expandableListView = navigationView.findViewById(R.id.expandable_list_view);
+        //ExpandableListView expandableListView = navigationView.findViewById(R.id.expandable_list_view);
         //expandableListView.
         View headerView =  navigationView.getHeaderView(0);
         ((TextView) headerView.findViewById(R.id.userFullNameTV)).setText(userDataClass.getUsername());
@@ -129,29 +128,49 @@ public class MainActivity extends AppCompatActivity
             CarInfoClass currentItem = carInfoClass.get(i);
             markers [i] = mMap.addMarker(new MarkerOptions().position(new LatLng(currentItem.getLat(), currentItem.getLng())).title(currentItem.getCarPlate()));
             hasUpdateFinished[i] = true;
+            markerCoordinates.append(i,new LatLng(currentItem.getLat(), currentItem.getLng()));
         }
+        new Thread(()->{
+            ArrayList<CarInfoClass> carsInfoClass;
+            LatLng latLng;
+            while (true){
+            try {
+                carsInfoClass = new UpdateLocationClass().getUpdates(branchId);
+                if ((carsInfoClass.size() > 0)) {
+                    for (int i = 0; i < carsInfoClass.size(); i++)
+                        for (int j = 0; j < carInfoClass.size(); j++)
+                            if (carInfoClass.get(j).getObjectId() == carsInfoClass.get(i).getObjectId()) {
+                                latLng = new LatLng(carsInfoClass.get(i).getLat(), carsInfoClass.get(i).getLng());
+                                markerCoordinates.setValueAt(j, latLng);
+                            }
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.printStackTrace();
+            }
+        }
+        }).start();
         new Thread(()->
         {
-            //float a = 45, b = 45;
-            ArrayList<CarInfoClass> carInfoClasses;
+            //ArrayList<CarInfoClass> carsInfoClass;
             while (true)
             {
                 try {
-                    UpdateLocationClass updateLocationClass = new UpdateLocationClass();
-                    carInfoClasses = updateLocationClass.getUpdates(3);
-                    Thread.sleep(1);
-                    if ((carInfoClasses.size() > 0)) {
-                        for (int i = 0; i < carInfoClasses.size(); i++)
+                    //carsInfoClass = new UpdateLocationClass().getUpdates(3);
+                    //Thread.sleep(1);
+                    //if ((carsInfoClass.size() > 0)) {
+                        //for (int i = 0; i < carsInfoClass.size(); i++)
                             for(int j = 0; j < carInfoClass.size(); j++)
-                            if(carInfoClass.get(j).getObjectId()==carInfoClasses.get(i).getObjectId())
+                            //if(carInfoClass.get(j).getObjectId()==carsInfoClass.get(i).getObjectId())
                             if (hasUpdateFinished[j])
                             {
                                 int currentElement = j;
-                                latLng = new LatLng(carInfoClasses.get(i).getLat(), carInfoClasses.get(i).getLng());
+                                //latLng = new LatLng(carsInfoClass.get(i).getLat(), carsInfoClass.get(i).getLng());
                                 hasUpdateFinished[currentElement] = false;
-                                handler.post(() -> UpdateMarker(markers[currentElement], latLng, currentElement));
+                                handler.post(() -> UpdateMarker(markers, markerCoordinates.valueAt(currentElement), currentElement));
                             }
-                    }
+                    //}
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -160,18 +179,17 @@ public class MainActivity extends AppCompatActivity
         //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
-    public void UpdateMarker(Marker marker, LatLng finalPosition,int i)
+    public void UpdateMarker(Marker [] markers, LatLng finalPosition,int i)
     {
-        final LatLng startPosition = marker.getPosition();
+        final LatLng startPosition = markers[i].getPosition();
         final Handler handler = new Handler();
         final long start = SystemClock.uptimeMillis();
         final Interpolator interpolator = new LinearInterpolator();
-        final float duration = 5500;
+        final float duration = 4000;
         //double updateStepLat = finalPosition.latitude-startPosition.latitude;
         //double updateStepLng = finalPosition.longitude-startPosition.longitude;
 
         handler.post(new Runnable() {
-
             @Override
             public void run() {
                 long elapsed = SystemClock.uptimeMillis() - start;
@@ -181,7 +199,7 @@ public class MainActivity extends AppCompatActivity
                         * startPosition.longitude;
                 double lat = t * finalPosition.latitude + (1 - t)
                         * startPosition.latitude;
-                marker.setPosition(new LatLng(lat, lng));
+                markers[i].setPosition(new LatLng(lat, lng));
                 // Repeat till progress is complete.
                 if (t < 1) {
                 // Post again 16ms later.
