103fc9a9573a753fa3c4c795a43a348ac119b33c
m7Rahm <Jack10wil>
2019-05-17 13:46:03 +0400

    Restaurant app Added

---
+++ WpfApp1/WpfApp1/SqlData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WpfApp1
+{
+    public class SqlData
+    {
+        SqlDataAdapter sqlProductsDataAdapter;
+        SqlDataAdapter sqlUnitsDataAdapter;
+        SqlDataAdapter sqlDataProvidersAdapter;
+        public SqlData(SqlConnection sqlConnection)
+        {
+            sqlDataProvidersAdapter = new SqlDataAdapter("select * from c_malsatanlar", sqlConnection);
+            sqlProductsDataAdapter = new SqlDataAdapter("select ad,id from c_mallar where is_active!=1", sqlConnection);
+            sqlUnitsDataAdapter = new SqlDataAdapter("select * from c_units", sqlConnection);
+
+
+        }
+        public DataTable GetProductsDataTable()
+        {
+            DataTable dataTable = new DataTable();
+            sqlProductsDataAdapter.Fill(dataTable: dataTable);
+            return dataTable;
+        }
+        public DataTable GetUnitsDataTable()
+        {
+            DataTable dataTable = new DataTable();
+            sqlUnitsDataAdapter.Fill(dataTable: dataTable);
+            return dataTable;
+        }
+        public DataTable GetProvidersDataTable()
+        {
+            DataTable dataTable = new DataTable();
+            sqlDataProvidersAdapter.Fill(dataTable: dataTable);
+            return dataTable;
+        }
+    }
+}
